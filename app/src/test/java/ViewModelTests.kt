1db7c61540814b12a7d6f872b434c70cff147a89
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-10-26 19:09:37 +0300

    Test price update

--- app/src/test/java/ViewModelTests.kt
+++ app/src/test/java/ViewModelTests.kt
@@ -3,6 +3,7 @@ import com.example.cupcake.model.OrderViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
+import java.text.NumberFormat
 
 class ViewModelTests {
     @get:Rule
@@ -16,6 +17,16 @@ class ViewModelTests {
         viewModel.quantity.observeForever {}
         viewModel.setQuantity(12)
         assertEquals(12, viewModel.quantity.value)
+    }
+
+    @Test
+    fun price_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.price.observeForever {}
+        viewModel.setQuantity(12)
+        val formatter: NumberFormat = NumberFormat.getCurrencyInstance()
+        formatter.maximumFractionDigits = 2
+        assertEquals(formatter.format(24.00), viewModel.price.value)
     }
 
 }
