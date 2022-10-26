6a610ff6289c3bc1aaae3d65eb907045170bc689
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-10-26 18:41:13 +0300

    Write first LiveData unit test

--- app/src/test/java/ViewModelTests.kt
+++ app/src/test/java/ViewModelTests.kt
@@ -1,3 +1,21 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.cupcake.model.OrderViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
 class ViewModelTests {
+    @get:Rule
+    // Specify that LiveData objects should not call the main thread.
+    // We need to provide a specific test rule any time we are testing a LiveData object.
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun quantity_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.quantity.observeForever {}
+        viewModel.setQuantity(12)
+        assertEquals(12, viewModel.quantity.value)
+    }
 
 }
